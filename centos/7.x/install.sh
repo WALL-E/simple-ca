ed232b26c8defeb4b0fd1776d7ca52b801324ec2
zhangzheng <zhangzheng@tokenxy.cn>
2018-09-20 17:48:57 +0800

    更新安装openssl version 1.1

--- centos/7.x/install.sh
+++ centos/7.x/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -x
+set -e
 
-sudo yum -y install openssl
-sudo yum -y install openssl-devel
+wget https://www.openssl.org/source/openssl-1.1.0f.tar.gz
+tar zxvf openssl-1.1.0f.tar.gz
+cd openssl-1.1.0f
+./config
+sudo make
+sudo make install
+sudo ln -s /usr/local/lib64/libssl.so.1.1 /usr/lib64/
+sudo ln -s /usr/local/lib64/libcrypto.so.1.1 /usr/lib64/
+
+/usr/local/bin/openssl version
