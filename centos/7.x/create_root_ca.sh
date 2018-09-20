58ecede2e1fa698413a9ef89529b6e1c52e0c065
zhangzheng <zhangzheng@tokenxy.cn>
2018-09-20 18:20:56 +0800

    use ed25519

--- centos/7.x/create_root_ca.sh
+++ centos/7.x/create_root_ca.sh
@@ -9,8 +9,8 @@ sudo /bin/cp -fr ./template/openssl.cnf /etc/pki/tls/
 
 cd /etc/pki/CA
 
-sudo openssl genrsa -out ./private/cakey.pem
+sudo /usr/local/bin/openssl genpkey -algorithm Ed25519 -out ./private/cakey.pem
 
-# sudo openssl req -new -days 3650 -key ./private/cakey.pem -out careq.pem
-# sudo openssl ca -selfsign -in careq.pem -out ./private/cacert.pem
-sudo openssl req -new -x509 -days 3650 -key ./private/cakey.pem -out ./cacert.pem
+# sudo openssl req -new -days 3650 -key ./private/cakey.pem -out careq.pem -config /etc/pki/tls/openssl.cnf
+# sudo openssl ca -selfsign -in careq.pem -out ./private/cacert.pem -config /etc/pki/tls/openssl.cnf
+sudo /usr/local/bin/openssl req -new -x509 -days 3650 -key ./private/cakey.pem -out ./cacert.pem -config /etc/pki/tls/openssl.cnf
--- centos/7.x/create_user_key_cert.sh
+++ centos/7.x/create_user_key_cert.sh
@@ -6,7 +6,7 @@ set -x
 SERIAL=`cat /etc/pki/CA/serial`
 NAME="user_"$SERIAL
 
-sudo openssl genrsa -out $NAME.key 2048
-# sudo openssl req -new -days 3650 -key $NAME.key -out $NAME.csr
-sudo openssl req -new -days 3650 -key $NAME.key -out $NAME.csr -subj /C=CN/ST=BJ/O=Company.Ltd/OU=QOS/CN=QOS$SERIAL
-sudo openssl ca -in $NAME.csr -out $NAME.crt
+sudo /usr/local/bin/openssl genpkey -algorithm Ed25519 -out $NAME.key
+# sudo /usr/local/bin/openssl req -new -days 3650 -key $NAME.key -out $NAME.csr -config /etc/pki/tls/openssl.cnf
+sudo /usr/local/bin/openssl req -new -days 3650 -key $NAME.key -out $NAME.csr -subj /C=CN/ST=BJ/O=Company.Ltd/OU=QOS/CN=QOS$SERIAL -config /etc/pki/tls/openssl.cnf
+sudo /usr/local/bin/openssl ca -in $NAME.csr -out $NAME.crt -config /etc/pki/tls/openssl.cnf
