903294e91fac70a6ead8c25df92f1c101f037ff6
zhangzheng <zhangzheng@tokenxy.cn>
2018-09-28 20:52:59 +0800

    修改脚本代码风格

--- centos/7.x/create_user_key_cert.sh
+++ centos/7.x/create_user_key_cert.sh
@@ -13,6 +13,11 @@ CRT=$SERIAL".crt"
 sudo /usr/local/bin/openssl genpkey -algorithm Ed25519 -outform PEM -out $PRI
 sudo /usr/local/bin/openssl pkey -in $PRI -outform PEM -pubout -out $PUB
 
-# sudo /usr/local/bin/openssl req -new -days 3650 -key $NAME.key -out $NAME.csr -config /etc/pki/tls/openssl.cnf
-sudo /usr/local/bin/openssl req -new -days 3650 -key $PRI -out $CSR -subj /C=CN/ST=BJ/O=Company.Ltd/OU=QOS/CN=QOS$SERIAL -config /etc/pki/tls/openssl.cnf
+sudo /usr/local/bin/openssl req -new \
+-days 3650 \
+-key $PRI \
+-out $CRT \
+-subj /C=CN/ST=BJ/O=Company.Ltd/OU=QOS/CN=QOS$SERIAL/emailAddress=owner@QOS$SERIAL.QOS \
+-config /etc/pki/tls/openssl.cnf
+
 sudo /usr/local/bin/openssl ca -in $CSR -out $CRT -config /etc/pki/tls/openssl.cnf
