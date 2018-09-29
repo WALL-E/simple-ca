766d20931c8e7b6ef8ff7bf7445e58abf1475475
zhangzheng <zhangzheng@tokenxy.cn>
2018-09-29 09:51:49 +0800

    默认证书有效期是7天

--- centos/7.x/create_user_key_cert.sh
+++ centos/7.x/create_user_key_cert.sh
@@ -14,7 +14,7 @@ sudo /usr/local/bin/openssl genpkey -algorithm Ed25519 -outform PEM -out $PRI
 sudo /usr/local/bin/openssl pkey -in $PRI -outform PEM -pubout -out $PUB
 
 sudo /usr/local/bin/openssl req -new \
--days 3650 \
+-days 7 \
 -key $PRI \
 -out $CSR \
 -subj /C=CN/ST=BJ/O=Company.Ltd/OU=QOS/CN=QOS$SERIAL/emailAddress=owner@QOS$SERIAL.QOS \
